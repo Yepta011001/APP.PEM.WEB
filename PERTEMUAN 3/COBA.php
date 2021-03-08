d8db466c8420ed6d1d443d554290338e5898b6f0
Yepta011001 <80236753+Yepta011001@users.noreply.github.com>
2021-03-08 11:17:30 +0700

    Add files via upload

---
+++ PERTEMUAN 3/COBA.php
@@ -0,0 +1,10 @@
+<?php
+
+	$namadepan = "Eko";
+	$namabelakang = "Siswanto";
+	$jumlahbarang = 3;
+	$harga = 100000;
+	
+	echo "Nama saya adalah $namadepan $namabelakang. Saya membeli buah naga sebanyak $jumlahbarang seharga $harga";
+
+?>
